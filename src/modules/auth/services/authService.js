aa39a4830b104373d42baebce3490d7939c9d999
gerssonoliva <gerssonoliva@gmail.com>
2026-05-14 18:57:45 -0500

    ADVANCE SECTION 12

---
+++ src/modules/auth/services/authService.js
@@ -0,0 +1,52 @@
+import apiClient from "@/api/axiosConfig";
+
+export default {
+
+  /* async login(credentials) {
+    await apiClient.post("/auth/login", {
+      email: credentials.email,
+      password: credentials.password,
+    }).then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, */
+
+  async login(credentials) {
+    try {
+      const response = await apiClient.post("/auth/login", credentials);
+      return response.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  },
+
+  async logout() {
+    try {
+      const response = await apiClient.post("/auth/logout");
+      return response.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  },
+
+  async refresh() {
+    try {
+      const response = await apiClient.post("/auth/refresh");
+      return response.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  },
+
+  async me() {
+    try {
+      const response = await apiClient.post("/auth/me");
+      return response.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+}
